e879697a47504e684cf61b6074376c96fe0b1ea5
Chris <Chris@MacBook-Pro-5.fios-router.home>
2020-11-11 19:53:57 -0500

    Improved Strategy tab and added personal GitHub links

--- views/components/NavBar.tsx
+++ views/components/NavBar.tsx
@@ -12,9 +12,16 @@ const NavBar = (props: any) => {
           <div className='buttons' id='navStrategies' onClick={() => {props.setPage('strategies')}}>Strategies</div>
           <div className='buttons' id='navDocs' onClick={() => {props.setPage('docs')}}>Docs</div>
         </div>
-        <footer><span id='developedBy'>Developed by </span>{credits}</footer>
+        <footer><span id='developedBy'>Developed by </span><a className="githubs" href="https://github.com/chcho2" target="_blank">Connie Cho</a>, <a className="githubs" href="https://github.com/alicejfu" target="_blank">Alice Fu</a>, <a className="githubs" href="https://github.com/opennoise1" target="_blank">Chris Kopcow</a>, <a className="githubs" href="https://github.com/gkruchin" target="_blank">George Kruchinina</a> and <a className="githubs" href="https://github.com/leeced94" target="_blank">Cedric Lee</a></footer>
       </div>
   )
 }
 
+// <a href="https://github.com/leeced94" target="_blank">Cedric Lee</a>
+
+// https://github.com/leeced94
+// https://github.com/alicejfu
+// https://github.com/opennoise1
+// https://github.com/gkruchin
+// https://github.com/chcho2
 export default NavBar;
